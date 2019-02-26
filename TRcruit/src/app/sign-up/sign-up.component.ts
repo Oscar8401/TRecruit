eb60169d0b571911ed4bd1ddc0ff5f921806a3c6
Taleb_84 <taleb_n1@hotmail.com>
2019-02-26 22:18:17 +0100

    #create the login form

---
+++ TRcruit/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/user.model';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+  user: User;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
